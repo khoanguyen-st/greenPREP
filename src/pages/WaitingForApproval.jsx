0e128bf204c0d09a3746d61fca7faf096fddcd17
Nguyễn Phước Trọng <phuoctrong1001@gmail.com>
2025-03-24 10:16:14 +0700

    feat: [SCRUM-35] waiting for approval screen

--- src/pages/WaitingForApproval.jsx
+++ src/pages/WaitingForApproval.jsx
@@ -1,26 +1,20 @@
+// @ts-nocheck
 import { Layout, Typography, Image } from 'antd'
 import SharedHeader from '../shared/ui/SharedHeader'
+import WaitingGif from '../assets/Images/waiting.gif'
 
 const { Content } = Layout
 const { Title, Text } = Typography
 
 const WaitingForApproval = () => {
   return (
-    <Layout style={{ minHeight: '100vh' }}>
+    <Layout className="min-h-screen">
       <SharedHeader />
-      <Content style={{ padding: '16px', textAlign: 'center' }}>
-        <div style={{ marginTop: '50px' }}>
+      <Content className="p-4 text-center">
+        <div className="mt-12">
           <Title level={2}>Your request is in the teacher&apos;s hands!</Title>
-          <Text style={{ fontSize: '18px', display: 'block', marginTop: '8px' }}>
-            Sit tight and hold on for a moment!
-          </Text>
-          <Image
-            src="src/assets/Images/waiting.gif"
-            alt="Notification"
-            preview={false}
-            width={200}
-            style={{ marginTop: '16px' }}
-          />
+          <Text className="mt-2 block text-2xl">Sit tight and hold on for a moment!</Text>
+          <Image src={WaitingGif} alt="Notification" preview={false} width={319} height={341} className="mt-12" />
         </div>
       </Content>
     </Layout>
