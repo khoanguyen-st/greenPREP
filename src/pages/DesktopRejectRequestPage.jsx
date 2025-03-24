5d7a895da0b23b8acc213e84d7192b1f914ce8be
Harry <107847294+trongnghia9@users.noreply.github.com>
2025-03-24 10:16:36 +0700

    feat: [SCRUM-36] Implement Rejected request screen

--- src/pages/DesktopRejectRequestPage.jsx
+++ src/pages/DesktopRejectRequestPage.jsx
@@ -1,54 +1,48 @@
-import { Component } from 'react'
 import SharedHeader from "@shared/ui/SharedHeader";
-import { Layout, Typography, Button, Space } from "antd";
+import { Layout, Typography, Button, Space, Image } from "antd";
+import { useNavigate } from "react-router-dom";
 const { Title, Text } = Typography;
 const { Content } = Layout;
 
-export default class DesktopRejectRequestPage extends Component {
-  render() {
-    return (
-      <Layout style={{ height: '100vh', overflow: 'hidden' }}>
-        <SharedHeader />
-        <Content className="bg-white">
-          <div className="flex flex-col items-center justify-center" style={{ height: 'calc(100vh - 64px)' }}>
-            <Space direction="vertical" align="center" size="large">
-              <Title level={1} style={{ marginBottom: 0, textAlign: 'center' }}>
-                Oopsss! Your request has been denied 
-              </Title>
-              
-              <Text style={{ fontSize: '1.1rem', textAlign: 'center' }}>
-                Please reach out to your teacher for support!
-              </Text>
+const DesktopRejectRequestPage = () => {
+  const navigate = useNavigate();
+  
+  return (
+    <Layout style={{ height: '100vh', overflow: 'hidden' }}>
+      <SharedHeader />
+      <Content className="bg-white">
+        <div className="flex flex-col items-center justify-center" style={{ height: 'calc(100vh - 64px)' }}>
+          <Space direction="vertical" align="center" size="large">
+            <Title level={1} style={{ marginBottom: 0, textAlign: 'center' }}>
+              Oopsss! Your request has been denied 
+            </Title>
+            
+            <Text style={{ fontSize: '1.1rem', textAlign: 'center' }}>
+              Please reach out to your teacher for support!
+            </Text>
 
-              <img 
-                src="src\assets\Images\icon_re.jpg"
-                alt="Reject Request Icon"
-                style={{ 
-                  width: 'auto',
-                  height: 'auto',
-                  maxWidth: '200px',
-                  maxHeight: '200px',
-                  objectFit: 'contain'
-                }}
-              />
+            <Image
+              src="/src/assets/Images/icon_re.jpg"
+              alt="Reject Request Icon"
+              preview={false}
+              width={283.48}
+              height={355}
+              className="object-contain"
+            />
 
-              <Button 
-                type="primary"
-                size="large"
-                onClick={() => window.location.href = './homepage'}
-                style={{
-                  backgroundColor: '#1e3a8a', 
-                  height: 'auto',
-                  padding: '10px 30px',
-                  fontSize: '1.1rem'
-                }}
-              >
-                Go Back Home
-              </Button>
-            </Space>
-          </div>
-        </Content>
-      </Layout>
-    )
-  }
+            <Button 
+              type="primary"
+              size="large"
+              onClick={() => navigate('/homepage')}
+              className="bg-blue-900 h-auto px-8 py-2.5 text-lg"
+            >
+              Go Back Home
+            </Button>
+          </Space>
+        </div>
+      </Content>
+    </Layout>
+  )
 }
+
+export default DesktopRejectRequestPage
