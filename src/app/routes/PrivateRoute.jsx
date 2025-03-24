337de3a24d4b6a3e24265bc63de58a2ec7ce3c16
ndt0403 <ndt2704tndv@gmail.com>
2025-03-24 16:03:59 +0700

    feat: [SCRUM-8] Create Speaking introduction and instruction screen layout

--- src/app/routes/PrivateRoute.jsx
+++ src/app/routes/PrivateRoute.jsx
@@ -1,33 +1,34 @@
-import HomePage from "@pages/HomePage.jsx";
-import WritingTestPage from "@pages/WritingTestPage.jsx";
-import IntroductionScreen from '@pages/IntroductionScreen';
-import { ProtectedRoute } from "./ProtectedRoute/ProtectedRoute.jsx";
-import ListeningPage from "@pages/ListeningPage.jsx";
+import HomePage from '@pages/HomePage.jsx'
+import WritingTestPage from '@pages/WritingTestPage.jsx'
+import IntroductionScreen from '@pages/IntroductionScreen'
+import { ProtectedRoute } from './ProtectedRoute/ProtectedRoute.jsx'
+import ListeningPage from '@pages/ListeningPage.jsx'
+import SpeakingPage from '@pages/SpeakingPage.jsx'
 
 const PrivateRoute = [
   {
-    path: "/",
+    path: '/',
     element: <ProtectedRoute />,
     children: [
       {
-        path: "homepage",
-        element: <HomePage />,
+        path: 'homepage',
+        element: <HomePage />
+      },
+      { path: 'speaking', element: <SpeakingPage /> },
+      {
+        path: 'listening',
+        element: <ListeningPage />
       },
       {
-        path: "listening",
-        element: <ListeningPage />,
+        path: 'writing',
+        element: <WritingTestPage />
       },
       {
-        path: "writing",
-        element: <WritingTestPage />,
-      },
-      {
-        path: "introduction",
-        element: <IntroductionScreen/>      
-      },
-    ],
-  },
-  
-];
+        path: 'introduction',
+        element: <IntroductionScreen />
+      }
+    ]
+  }
+]
 
-export default PrivateRoute;
+export default PrivateRoute
