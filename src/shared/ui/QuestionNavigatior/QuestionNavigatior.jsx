8aed920501a5fd0b3b809c9e3a1dda80b38585b8
phucGCD210651 <phucpvp7@gmail.com>
2025-03-24 09:48:17 +0700

    fix: [SCRUM-63] layout break

--- src/shared/ui/QuestionNavigatior/QuestionNavigatior.jsx
+++ src/shared/ui/QuestionNavigatior/QuestionNavigatior.jsx
@@ -14,7 +14,7 @@ export default function QuestionNavigator({ values, action, position }) {
             hover:outline hover:outline-2 hover:rounded-md hover:outline-gray-500 hover:cursor-pointer`}
         >
           {(type === "flagged" || type === "answered-flagged") && (
-            <div className="absolute top-[-0.031rem] right-0 w-[0.875rem] h-[0.875rem]">
+            <div className="absolute right-0 top-[-0.5px] flex h-[14px] w-[14px] items-center justify-center">
               <svg
                 className="w-full h-full"
                 viewBox="0 0 14 14"
