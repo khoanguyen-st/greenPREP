347e9e057afb85fb8c727ded65204f69c2aa5883
Quoc Nguyen <quoc.nguyen@stunited.vn>
2025-03-26 09:53:15 +0700

    fix package

--- src/features/writing/ui/writing-question-form.jsx
+++ src/features/writing/ui/writing-question-form.jsx
@@ -1,7 +1,7 @@
-import { Form, Input, Typography } from "antd";
-import FlagButton from "@shared/ui/FLagButton/FlagButton";
+import { Form, Input, Typography } from 'antd'
+import FlagButton from '@shared/ui/FLagButton/FlagButton'
 
-const { Text } = Typography;
+const { Text } = Typography
 
 const QuestionForm = ({
   currentPart,
@@ -12,21 +12,18 @@ const QuestionForm = ({
   handleTextChange,
   countWords,
   wordCounts,
-  DEFAULT_MAX_WORDS,
+  DEFAULT_MAX_WORDS
 }) => {
   return (
     <Form layout="vertical">
       {currentPart.Questions.map((question, index) => {
-        const fieldName = `answer-${currentPart.ID}-${index}`;
-        const maxWords =
-          partNumber === 1
-            ? null
-            : question.maxWords || DEFAULT_MAX_WORDS[partNumber];
+        const fieldName = `answer-${currentPart.ID}-${index}`
+        const maxWords = partNumber === 1 ? null : question.maxWords || DEFAULT_MAX_WORDS[partNumber]
         return (
           <Form.Item
             key={fieldName}
             label={
-              <div className="flex items-center justify-between w-[1000px] gap-2">
+              <div className="flex w-[1000px] items-center justify-between gap-2">
                 <div className="flex-1 pl-2 md:pl-0">{question.Content}</div>
                 <div className="smL:mr-[-12px]">
                   <FlagButton
@@ -37,19 +34,19 @@ const QuestionForm = ({
               </div>
             }
             name={fieldName}
-            initialValue={answers[fieldName] || ""}
+            initialValue={answers[fieldName] || ''}
             rules={[
               { required: false },
               ...(maxWords
                 ? [
                     {
                       validator: (_, value) =>
-                        countWords(value || "") > maxWords
+                        countWords(value || '') > maxWords
                           ? Promise.reject(`Maximum ${maxWords} words!`)
-                          : Promise.resolve(),
-                    },
+                          : Promise.resolve()
+                    }
                   ]
-                : []),
+                : [])
             ]}
           >
             <Input.TextArea
@@ -57,21 +54,21 @@ const QuestionForm = ({
               autoSize={{ minRows: 5, maxRows: 10 }}
               className="w-full"
               placeholder="Enter your answer here"
-              value={answers[fieldName] || ""}
-              onChange={(e) => handleTextChange(fieldName, e.target.value)}
+              value={answers[fieldName] || ''}
+              onChange={e => handleTextChange(fieldName, e.target.value)}
             />
             {maxWords && (
               <Text
-                className={`mt-1 block text-sm ${wordCounts[fieldName] > maxWords ? "text-red-500" : "text-gray-500"}`}
+                className={`mt-1 block text-sm ${wordCounts[fieldName] > maxWords ? 'text-red-500' : 'text-gray-500'}`}
               >
                 {`Word count: ${wordCounts[fieldName] || 0} / ${maxWords}`}
               </Text>
             )}
           </Form.Item>
-        );
+        )
       })}
     </Form>
-  );
-};
+  )
+}
 
-export default QuestionForm;
+export default QuestionForm
---
+++ src/features/writing/ui/writing-question-navigator-container.jsx
@@ -0,0 +1,54 @@
+import TimeRemaining from '@shared/ui/TimeRemaining/TimeRemaining'
+import QuestionNavigator from '@shared/ui/QuestionNavigatior/QuestionNavigatior'
+import { useState } from 'react'
+import { Button } from 'antd'
+import { MenuOutlined } from '@ant-design/icons'
+
+const QuestionNavigatorContainer = ({
+  data,
+  answers,
+  flaggedQuestions,
+  setCurrentPartIndex,
+  currentPartIndex,
+  handleSubmit
+}) => {
+  const [isNavigatorOpen, setIsNavigatorOpen] = useState(false)
+  return (
+    <div>
+      <Button
+        className="fixed bottom-[50%] right-5 rounded-full bg-blue-500 p-2 text-white shadow-lg md:hidden"
+        onClick={() => setIsNavigatorOpen(!isNavigatorOpen)}
+      >
+        <MenuOutlined />
+      </Button>
+      <div
+        className={`border-black-300 fixed right-2 z-50 h-auto w-60 rounded-lg border bg-white p-2 shadow-lg ${isNavigatorOpen ? 'block' : 'hidden'} bottom-[65%] md:block mdL:bottom-[70%]`}
+      >
+        <TimeRemaining duration={10 * 60} onAutoSubmit={handleSubmit} />
+        <QuestionNavigator
+          values={data.Parts.map(part => {
+            const isFlagged = part.Questions.some((_, index) => flaggedQuestions[`answer-${part.ID}-${index}`])
+            const isAnswered = Object.keys(answers).some(
+              key => key.startsWith(`answer-${part.ID}`) && answers[key]?.trim() !== ''
+            )
+
+            let type = 'unanswered'
+            if (isAnswered && isFlagged) {
+              type = 'answered-flagged'
+            } else if (isFlagged) {
+              type = 'flagged'
+            } else if (isAnswered) {
+              type = 'answered'
+            }
+
+            return { type }
+          })}
+          action={setCurrentPartIndex}
+          position={currentPartIndex}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default QuestionNavigatorContainer
