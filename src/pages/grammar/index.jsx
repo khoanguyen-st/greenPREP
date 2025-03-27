ff100b6eb667449172a74964edf2e38af38817a5
Trung Do <trung.do@enouvo.com>
2025-03-27 11:15:10 +0700

    fix: [SCRUM-00] update submit

--- src/pages/grammar/index.jsx
+++ src/pages/grammar/index.jsx
@@ -1,14 +1,10 @@
 import { GrammarProvider } from '@shared/context/grammar-context'
-import CustomAlert from '@shared/ui/CustomAlert/CustomAlert'
-import useAntiCheat from '@shared/utils/antiCheat'
 import { ConfigProvider } from 'antd'
 import { Outlet } from 'react-router-dom'
 
 import { GRAMMAR_DATA } from '@/__mock/grammar'
 
 export const GrammarPage = () => {
-  const { showAlert, alertMessage, enableFullScreen } = useAntiCheat()
-
   const theme = {
     token: {
       colorPrimary: '#003087',
@@ -21,7 +17,6 @@ export const GrammarPage = () => {
     <>
       <GrammarProvider data={GRAMMAR_DATA}>
         <ConfigProvider theme={theme}>
-          <CustomAlert show={showAlert} onConfirm={enableFullScreen} message={alertMessage} />
           <Outlet />
         </ConfigProvider>
       </GrammarProvider>
