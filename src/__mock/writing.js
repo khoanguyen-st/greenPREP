1f524fb2e0da310990076f5a7e01eb1f7cba0fe0
BaoLam2K3 <60538673+BaoLam2K3@users.noreply.github.com>
2025-03-27 09:30:46 +0700

    fix: [SCRUM-107]-refator

--- src/__mock/writing.js
+++ src/__mock/writing.js
@@ -1,37 +1,37 @@
 export const WRITING_DATA = {
   title: 'Reading Practice Test',
   testName: 'Aptis General Practice Test',
-  section: 'Reading',
+  section: 'Writing',
   testStructure: {
-    desc: 'This test has 4 parts, you will have 30 minutes to complete the test.',
+    desc: 'This test has 4 parts, you will have 50 minutes to complete the test.',
     parts: [
       {
-        name: 'Part 1: Sentence Completions',
+        name: 'Part 1: 3 mins',
         desc: 'In this part, you will read a sentence with a missing word. You will have to choose the best word to fill the gap from the options provided.'
       },
       {
-        name: 'Part 2: Multiple Choice',
+        name: 'Part 2: 7 mins',
         desc: 'In this part, you will read a text and answer multiple choice questions.'
       },
       {
-        name: 'Part 3: Multiple Matching',
+        name: 'Part 3: 10 mins',
         desc: 'In this part, you will read a text and match the questions to the correct answers.'
       },
       {
-        name: 'Part 4: Reading and Writing',
+        name: 'Part 4: 30 mins',
         desc: 'In this part, you will read a text and write a short answer to a question.'
       }
     ]
   },
   fornDescription: {
     desc: 'In this test, you will read a variety of texts and answer questions about them. The texts will be taken from a range of sources, such as newspapers, magazines, and books. You will need to read the texts carefully and answer the questions based on what you have read.',
-    duration: '30 minutes',
+    duration: '50 minutes',
     numberOfQuestions: '20 questions'
   },
   notes: {
     title: 'Notes',
     listNotes: [
-      'You will have 30 minutes to complete the test.',
+      'You will have 50 minutes to complete the test.',
       'You can go back and change your answers at any time.',
       'You can only submit your answers once.',
       'You will not be able to return to the test once you have submitted your answers.'
