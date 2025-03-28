b2fcdc336613ee487056b3fb6c83bec2d3675727
vyquaquay <vydapgiai69@gmail.com>
2025-03-28 15:21:11 +0700

    change style of the headphone check and import to main page

--- src/pages/listening/listening-introduction.jsx
+++ src/pages/listening/listening-introduction.jsx
@@ -1,3 +1,4 @@
+import HeadphoneCheck from '@pages/listening/listening-headphonecheck'
 import { useListeningData } from '@shared/context/listening-context'
 import { Introduction } from '@shared/ui/introduction'
 import { useNavigate } from 'react-router-dom'
@@ -6,10 +7,15 @@ const ListeningIntroduction = () => {
   const navigate = useNavigate()
   const data = useListeningData()
   const onStart = () => {
-    navigate('/listening/headphonecheck')
+    navigate('/listening/test')
   }
 
-  return <Introduction data={data} onStart={onStart} />
+  return (
+    <>
+      <Introduction data={data} onStart={onStart} />
+      <HeadphoneCheck />
+    </>
+  )
 }
 
 export default ListeningIntroduction
