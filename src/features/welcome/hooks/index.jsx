a11acd44b74617aa7431eddde4cbbe78157f2a40
phucGCD210651 <phucpvp7@gmail.com>
2025-04-09 17:43:41 +0700

    feat: [SCRUM-150] merged dev

---
+++ src/features/welcome/hooks/index.jsx
@@ -0,0 +1,24 @@
+import { getStudentSessionRequest, joinSession } from '@features/welcome/api'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+
+export const useJoinSession = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: joinSession,
+    onSuccess: () => {
+      // Invalidate or refetch any relevant queries if needed
+      queryClient.invalidateQueries({ queryKey: ['sessions'] })
+    },
+    onError: error => {
+      console.error('this is hook', error)
+    }
+  })
+}
+export const useGetStudentSessionRequest = ({ sessionId, userId, requestId }) => {
+  return useQuery({
+    queryKey: ['student-session-request', sessionId, userId, requestId],
+    queryFn: () => getStudentSessionRequest({ sessionId, userId, requestId }),
+    refetchInterval: 3000 // auto refetch every 3 seconds
+  })
+}
---
+++ src/features/welcome/ui/waiting-for-approval.jsx
@@ -0,0 +1,48 @@
+import { WaitingGif } from '@assets/images'
+import { useGetStudentSessionRequest } from '@features/welcome/hooks'
+import SharedHeader from '@shared/ui/base-header'
+import { Image, Layout, Typography } from 'antd'
+import { useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+
+const WaitingForApproval = () => {
+  const { Content } = Layout
+  const { Title, Text } = Typography
+  const navigate = useNavigate()
+  const { userId, sessionId, requestId } = useParams()
+
+  const { data, isLoading } = useGetStudentSessionRequest({ sessionId, userId, requestId })
+
+  useEffect(() => {
+    if (!isLoading && data) {
+      const sessionRequest = data?.data?.sessionRequest
+      const sessionPaticipantId = data?.data?.sessionParticipant?.ID
+      const status = sessionRequest?.status
+
+      // Save to localStorage
+      localStorage.setItem('sessionPaticipantId', JSON.stringify(sessionPaticipantId))
+
+      if (status === 'approved') {
+        navigate('/introduction')
+      }
+      if (status === 'rejected') {
+        navigate('/rejected')
+      }
+    }
+  }, [data, isLoading, navigate])
+
+  return (
+    <Layout className="min-h-screen">
+      <SharedHeader />
+      <Content className="p-4 text-center">
+        <div className="mt-12">
+          <Title level={2}>Your request is in the teacher&apos;s hands!</Title>
+          <Text className="mt-2 block text-2xl">Sit tight and hold on for a moment!</Text>
+          <Image src={WaitingGif} alt="Notification" preview={false} width={319} height={341} className="mt-12" />
+        </div>
+      </Content>
+    </Layout>
+  )
+}
+
+export default WaitingForApproval
