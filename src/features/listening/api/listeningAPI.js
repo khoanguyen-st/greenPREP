cd97548fb49294b5628e8557e044475779117893
BaoLam2K3 <60538673+BaoLam2K3@users.noreply.github.com>
2025-04-11 17:36:25 +0700

    fix: [SCRUM-191] fix api base url

--- src/features/listening/api/listeningAPI.js
+++ src/features/listening/api/listeningAPI.js
@@ -1,7 +1,6 @@
 import axios from '@shared/config/axios'
 
-const API_BASE_URL = import.meta.env.VITE_TEST_BASE_URL
-const API_SAVE_ANSWERS = import.meta.env.VITE_BASE_URL
+const API_BASE_URL = import.meta.env.VITE_BASE_URL
 
 export const fetchListeningTestDetails = async () => {
   const response = await axios.get(`${API_BASE_URL}/topics/ef6b69aa-2ec2-4c65-bf48-294fd12e13fc`, {
@@ -14,7 +13,7 @@ export const fetchListeningTestDetails = async () => {
 
 export const saveListeningAnswers = async formattedAnswers => {
   try {
-    const response = await axios.post(`${API_SAVE_ANSWERS}/student-answers`, formattedAnswers)
+    const response = await axios.post(`${API_BASE_URL}/student-answers`, formattedAnswers)
     return response.data
   } catch (error) {
     console.error('Error saving listening answers:', error)
