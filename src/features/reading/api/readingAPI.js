e26480141473820b38071f4052736d6a6332bebf
Trần Huy Hoàng <hoanghuytran10102003@gmail.com>
2025-04-15 11:39:13 +0700

    feat: [SCRUM-194] integrate with submit student answers  and get topic api

--- src/features/reading/api/readingAPI.js
+++ src/features/reading/api/readingAPI.js
@@ -1,12 +1,28 @@
 import axiosInstance from '@shared/config/axios'
 
-const API_BASE_URL = 'https://greenprep-api.onrender.com/api'
+const API_BASE_URL = import.meta.env.VITE_BASE_URL
 
 export const fetchReadingTestDetails = async () => {
-  const response = await axiosInstance.get(`${API_BASE_URL}/topics/ef6b69aa-2ec2-4c65-bf48-294fd12e13fc`, {
+  const globalData = JSON.parse(localStorage.getItem('globalData'))
+
+  if (!globalData?.topicId) {
+    throw new Error('Topic ID not found in global data')
+  }
+
+  const response = await axiosInstance.get(`${API_BASE_URL}/topics/${globalData.topicId}`, {
     params: {
       skillName: 'READING'
     }
   })
   return response.data
+}
+
+export const submitStudentAnswers = async submitData => {
+  try {
+    const response = await axiosInstance.post(`${API_BASE_URL}/student-answers`, submitData)
+    return response.data
+  } catch (error) {
+    console.error('Error submitting student answers:', error)
+    throw error
+  }
 }
